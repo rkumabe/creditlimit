64b392f394b8ac6518f8fe97457b4f247af5c554
RODRIGO TOMIO KUMABE <rodrigo.kumabe@egs.com.br>
2019-02-14 19:23:06 -0200

    first commit

---
+++ src/main/java/com/zagwork/creditlimit/CreditlimitApplication.java
@@ -0,0 +1,14 @@
+package com.zagwork.creditlimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditlimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditlimitApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/zagwork/creditlimit/CreditlimitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zagwork.creditlimit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditlimitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
